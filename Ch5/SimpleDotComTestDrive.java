6bab97053f258731ac31e522f5648ccf3aa9340e
K2LU <rigansust@gmail.com>
2021-05-10 00:41:29 +0600

    changes made in simeple dot game

--- Ch5/SimpleDotComTestDrive.java
+++ Ch5/SimpleDotComTestDrive.java
@@ -1,10 +1,25 @@
 public class SimpleDotComTestDrive  {
 
     public static void main (String [] args)    {
-        SimpleDotCom dot = new SimpleDotCom();
-        int[] locations = {2,3,4};
-        dot.setLocationsCells(locations);
-        String userGuess = "2";
-        String result = dot.checkYourself(userGuess);
+        int numOfGuesses = 0;
+
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNUM = (int) (Math.random() * 5);
+
+        int[] locations = {randomNUM, randomNUM+1, randomNUM+2};
+        theDotCom.setLocationsCells(locations);
+        boolean isAlive = true;
+
+        while(isAlive == true)  {
+            String guess = helper.getUserInpt("Enter a number: ");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses+=1;
+            if(result.equals("kill"))    {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses +" guesses");
+            }
+        }
     }
 }
